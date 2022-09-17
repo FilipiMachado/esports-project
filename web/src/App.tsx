da9d36ab4ddf82d0ddeec884b30c063be8c753de
fil <filipisee@hotmail.com>
2022-09-17 18:51:06 -0300

    finished main page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import "./styles/main.css";
 
 import logoImg from './assets/esports-logo.svg'
@@ -69,7 +71,8 @@ function App() {
             <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
           </div>
 
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded">
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
             Publicar anúncio
           </button>
         </div>
