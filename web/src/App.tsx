0dac8df02e94ab461f8556a04ff27ef682de2d17
fil <filipisee@hotmail.com>
2022-09-17 15:22:01 -0300

    configuring tailwind css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,9 @@
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <button>
-      { props.title }
-    </button>
-  )
-}
+import "./styles/main.css";
 
 function App() {
   return (
     <div>
-      <Button title="Send 1"/>
-      <Button title="Send 2"/>
-      <Button title="Send 3"/>
-      <Button title="Send 4"/>
+      <div>1</div>
     </div>
   );
 }
