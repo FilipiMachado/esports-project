109936f7efa5208818a2c735e485d8ff7bcdacc0
fil <filipisee@hotmail.com>
2022-09-17 17:12:13 -0300

    styling bottom content

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,31 +12,67 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-example.png" alt="" />
 
-          <div className="w-full pt-16 pb-4"></div>
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block ml-2">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1 ml-2">4 anúncios</span>
+          </div>
         </a>
-        <a href="" className="relative">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-example.png" alt="" />
-
+          
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block ml-2">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1 ml-2">4 anúncios</span>
+          </div>
         </a>
-        <a href="" className="relative">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-example.png" alt="" />
-
+          
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block ml-2">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1 ml-2">4 anúncios</span>
+          </div>
         </a>
-        <a href="" className="relative">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-example.png" alt="" />
-
+          
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block ml-2">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1 ml-2">4 anúncios</span>
+          </div>
         </a>
-        <a href="" className="relative">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-example.png" alt="" />
-
+          
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block ml-2">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1 ml-2">4 anúncios</span>
+          </div>
         </a>
-        <a href="" className="relative">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-example.png" alt="" />
-
+          
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block ml-2">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1 ml-2">4 anúncios</span>
+          </div>
         </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded">
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   );
