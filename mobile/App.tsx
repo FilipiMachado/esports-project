f35cee8e7d4f56222ec5315f19575303ed0db517
fil <filipisee@hotmail.com>
2022-09-17 14:20:44 -0300

    testing react native components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello React Native!</Text>
+      <Button title={"Clique Aqui!"}/>
       <StatusBar style="auto" />
     </View>
   );
