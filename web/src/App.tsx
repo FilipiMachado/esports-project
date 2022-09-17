de35d1df275050ec90528c7a229c03af652c3081
fil <filipisee@hotmail.com>
2022-09-17 15:43:08 -0300

    styling app component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import "./styles/main.css";
 
+import logoImg from './assets/esports-logo.svg'
+
 function App() {
   return (
-    <div>
-      <div>1</div>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center m-20">
+      <img src={logoImg} alt="main-logo" />
     </div>
   );
 }
