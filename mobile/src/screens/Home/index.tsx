ca7429a0879ce0f017643a34b2af31fd807555fd
fil <filipisee@hotmail.com>
2022-09-19 11:43:04 -0300

    finished mobile home page

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { View, Image } from "react-native";
+import { View, Image, FlatList } from "react-native";
 
 import { Header } from "../../components/Header";
+import { GameCard } from "../../components/GameCard";
+
 import logoImg from "../../assets/logo-nlw-esports.png";
+
+import { GAMES } from "../../utils/games";
 
 import { styles } from "./styles";
 
@@ -10,7 +14,18 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
-      <Header title={'Encontre seu duo!'} subtitle={'Selecione o game que deseja jogar...'}/>
+      <Header
+        title={"Encontre seu duo!"}
+        subtitle={"Selecione o game que deseja jogar..."}
+      />
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+      />
     </View>
   );
 }
