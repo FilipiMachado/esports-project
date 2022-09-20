0e3667df71e9a7d02fd816dffa5f1ff00cfd096b
fil <filipisee@hotmail.com>
2022-09-20 10:59:42 -0300

    componentizing web project

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -1,14 +1,19 @@
-function GameBanner() {
+interface GameBannerProps {
+  bannerURL: string;
+  title: string;
+  adsCount: number;
+}
+
+function GameBanner(props: GameBannerProps) {
   return (
     <a href="" className="relative rounded-lg overflow-hidden">
-      <img src="/game-example.png" alt="" />
-
+      <img src={props.bannerURL} alt="" />
       <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block ml-2">
-          League of Legends
+          {props.title}
         </strong>
         <span className="text-zinc-300 text-sm block mt-1 ml-2">
-          4 anúncios
+          {props.adsCount} anúncio(s)
         </span>
       </div>
     </a>
