e0bcbb64e820b30cefdaff2065700ba2ecab20b3
fil <filipisee@hotmail.com>
2022-09-20 15:25:51 -0300

    styling publish ad modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import "./styles/main.css";
 
@@ -52,7 +53,23 @@ function App() {
         })}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+
+          <Dialog.Content>
+            <Dialog.Content className="fixed bg-[#2A2834] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
+              <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
+              
+              <Dialog.Content>
+                <form>1</form>
+              </Dialog.Content>
+            </Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
