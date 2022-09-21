fd3da6fd048c3ad94649338ed892da1bf9afdb4d
fil <filipisee@hotmail.com>
2022-09-21 13:56:36 -0300

    structuring game info page

--- mobile/src/screens/Home/Game/index.tsx
+++ mobile/src/screens/Home/Game/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { TouchableOpacity, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRoute } from "@react-navigation/native";
+import { Entypo } from "@expo/vector-icons";
 
+import logoImg from "../../../assets/logo-nlw-esports.png";
 import { Background } from "../../../components/Background";
 
-import { styles } from "./styles";
-
 import { GameParams } from "../../../@types/navigation";
+
+import { styles } from "./styles";
+import { THEME } from "../../../theme";
 
 export function Game() {
   const route = useRoute();
@@ -14,7 +18,19 @@ export function Game() {
 
   return (
     <Background>
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Entypo
+              name="chevron-thin-left"
+              colors={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image source={logoImg} />
+        </View>
+      </SafeAreaView>
     </Background>
   );
 }
