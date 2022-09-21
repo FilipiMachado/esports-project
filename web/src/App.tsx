23642197a42b6b40f19d86afcf8d2b8364f17d40
fil <filipisee@hotmail.com>
2022-09-20 21:12:12 -0300

    finished form styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -66,7 +66,7 @@ function App() {
               Publique um anúncio
             </Dialog.Title>
 
-            <form className="mt-8">
+            <form className="mt-8 flex flex-col gap-4">
               <div className="flex flex-col gap-2">
                 <label className="font-semibold" htmlFor="game">
                   Qual o game?
@@ -78,7 +78,7 @@ function App() {
                 />
               </div>
 
-              <div>
+              <div className="flex flex-col gap-2">
                 <label htmlFor="name">Seu nome (ou nickname)</label>
                 <DefaultInput
                   id="name"
@@ -87,8 +87,8 @@ function App() {
                 />
               </div>
 
-              <div>
-                <div>
+              <div className="grid grid-cols-2 gap-6">
+                <div className="flex flex-col gap-2">
                   <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
                   <DefaultInput
                     id="yearsPlaying"
@@ -96,34 +96,88 @@ function App() {
                     placeholder="Tudo bem ser ZERO"
                   />
                 </div>
-                <div>
+                <div className="flex flex-col gap-2">
                   <label htmlFor="discord">Qual seu Discord?</label>
-                  <DefaultInput id="discord" type="text" placeholder="User#0000" />
+                  <DefaultInput
+                    id="discord"
+                    type="text"
+                    placeholder="User#0000"
+                  />
                 </div>
               </div>
 
-              <div>
-                <div>
+              <div className="flex gap-6">
+                <div className="flex flex-col gap-2">
                   <label htmlFor="weekDays">Quando costuma jogar?</label>
+
+                  <div className="grid grid-cols-4 gap-2">
+                    <button
+                      title="Domingo"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      D
+                    </button>
+                    <button
+                      title="Segunda"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      S
+                    </button>
+                    <button
+                      title="Terça"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      T
+                    </button>
+                    <button
+                      title="Quarta"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      Q
+                    </button>
+                    <button
+                      title="Quinta"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      Q
+                    </button>
+                    <button
+                      title="Sexta"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      S
+                    </button>
+                    <button
+                      title="Sábado"
+                      className="w-8 h-8 rounded bg-zinc-900"
+                    >
+                      S
+                    </button>
+                  </div>
                 </div>
-                <div>
+                <div className="flex flex-col gap-2 flex-1">
                   <label htmlFor="hourStart">Qual horário do dia?</label>
-                  <div>
+                  <div className="grid grid-cols-2 gap-2">
                     <DefaultInput id="hourStart" type="time" placeholder="De" />
                     <DefaultInput id="hourEnd" type="time" placeholder="Até" />
                   </div>
                 </div>
               </div>
 
-              <div>
+              <div className="mt-2 flex gap-2 text-sm">
                 <DefaultInput id="useVoiceChannel" type="checkbox" />
                 Costumo me conectar ao chat de voz
               </div>
 
-              <footer>
-                <button>Cancelar</button>
-                <button type="submit">
-                  <GameController />
+              <footer className="mt-4 flex justify-end gap-4">
+                <button className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600">
+                  Cancelar
+                </button>
+                <button
+                  type="submit"
+                  className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
+                >
+                  <GameController size={24} />
                   Encontrar duo
                 </button>
               </footer>
