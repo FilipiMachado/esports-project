462f86ddf64f70e9e74cb0d5ea07f4a8bc0c10e5
fil <filipisee@hotmail.com>
2022-09-21 14:14:34 -0300

    styling/structuring game info component

--- mobile/src/screens/Home/Game/index.tsx
+++ mobile/src/screens/Home/Game/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { TouchableOpacity, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
 
 import logoImg from "../../../assets/logo-nlw-esports.png";
 import { Background } from "../../../components/Background";
+import { Header } from "../../../components/Header";
 
 import { GameParams } from "../../../@types/navigation";
 
@@ -13,23 +14,33 @@ import { styles } from "./styles";
 import { THEME } from "../../../theme";
 
 export function Game() {
+  const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
 
   return (
     <Background>
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleGoBack}>
             <Entypo
               name="chevron-thin-left"
               colors={THEME.COLORS.CAPTION_300}
+              style={styles.icon}
               size={20}
             />
           </TouchableOpacity>
 
-          <Image source={logoImg} />
+          <Image source={logoImg} style={styles.logo} />
+
+          <View style={styles.right} />
         </View>
+
+        <Header title={game.title} subtitle="Conecte-se e comece a jogar!" />
       </SafeAreaView>
     </Background>
   );
--- mobile/src/screens/Home/Game/styles.ts
+++ mobile/src/screens/Home/Game/styles.ts
@@ -1,5 +1,26 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignContent: "center",
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 32,
+    marginTop: 28,
+    justifyContent: 'space-between',
+  }, 
+  icon: {
+    color: 'white',
+  },
+  logo: {
+
+  },
+  right: {
+    width: 20,
+    height: 20,
+  },
 });
